61456489a211b6c3951012c277c3202079801938
Samuel Souza <smksouza.dev@gmail.com>
2024-04-22 16:23:49 -0300

    Criação da tabela de candidatos

---
+++ gestao_vagas/src/main/java/br/com/samuel/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,28 @@
+package br.com.samuel.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+/**
+ * CandidateController
+ */
+
+@RestController()
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping()
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        
+        
+        System.out.println("Candidato");
+        System.out.println(candidateEntity.getEmail());
+    } 
+
+}
---
+++ gestao_vagas/src/main/java/br/com/samuel/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
@@ -0,0 +1,41 @@
+package br.com.samuel.gestao_vagas.modules.candidate.controllers;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Entity(name = "candidate")
+public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
+    @NotBlank
+    @Pattern(regexp = "\\s+", message = "O campo [username] não deve conter espaço")
+    private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+    
+    @Length(min = 6, max = 20, message = "A senha deve conter entre (6) e (20) caracteres")
+    private String password;
+    private String description;
+    private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
